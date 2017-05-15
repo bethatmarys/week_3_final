11bc7fbd55662defce568fc20b0ef3195d940deb
bethatmarys <Guest@Epicodus-9A.local>
2017-05-14 19:28:14 -0700

    add final jquery to git application to return correct result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,33 @@
 //Business Logic
-<<<<<<< HEAD
-var output = function(output) {
-// var result "userInput";
-for (index=1; index<=0; index++) {
-if (userInput % 15 === 0) {
-    makeArray.push("Ping Pong");
-} else if (userInput % 5 === 0) {
-    makeArray.push("Pong");
-} else if (userInput % 3 === 0) {
-    makeArray.push("Ping");
-} else {
-    makeArray.push(userInput);
-}
-};
-return makeArray;
-};
-$(function() {
-  $("form#formOne").submit(function(event) {
-    event.preventDefault();
-var userInput = ($("input#userInput").val());
-var result = output(userInput);
-result (bleep)
-$("result").append("<li>" + bleep + "</li>");
-});
-});
-=======
 var pingPong = function(number) {
   var result = [];
-  for (var i = 1; i <= number; i++ ) {
-  if (i % 15 === 0) {
-      result.push = ("Ping Pong");
-  } else if (i % 5 === 0) {
-      result.push = ("Pong");
-  } else if (i % 3 === 0) {
-      result.push = ("Ping");
-  } else {
-      result.push ("i");
-  }
-   return result;
+
+  for (var inputNumber = 1; inputNumber <= number; inputNumber++) {
+    if (inputNumber % 15 === 0) {
+        result.push("Ping Pong");
+    } else if (inputNumber % 5 === 0) {
+        result.push("Pong");
+    } else if (inputNumber % 3 === 0) {
+        result.push("Ping");
+    } else
+        result.push(" " + inputNumber);
+    };
+     return result;
 };
-}
+
 
 
 //Front End
 
 $(document).ready(function() {
   $("form#formOne").submit (function(event) {
+    //call a inputted number function and pass the number as an arguement
     event.preventDefault();
-    var pingPong = parseInt($("#number").val());
-    var result = pingPong();
-    result.forEach(function() {
-      $("#result").append("<li>" + listItem + "</li>");
+    //this will stop the function at the end of the run
+    var number = parseInt($("input#number").val());
+    //this tell the computer that the user input is a number
+    var result = pingPong(number);
+    //call the inputted number function and pass the number as a parameter
+      $("p").text(result);
     });
   });
-  });
->>>>>>> 3bed3bbd72e5842990467d0bf9b99fca81cd32fe
